1777763fe482aa1a1517a27e0d263772d6f25237
Peter Hodge <peterhodge@example.com>
2016-06-17 23:56:28 +1000

    Moved some stuff into homely.utils; removed 'terraform' references

--- homely/scripts/homely.py
+++ homely/scripts/homely.py
@@ -1,87 +1,21 @@
 #!/usr/bin/env python3
-# FIXME: use nice try/except to catch any import errors and provide a nice warning about modules
-# that are missing
 import os
 import subprocess
 import sys
-import contextlib
+
+from click import echo, group, argument, option
+
+from homely.utils import RepoError, RepoConfig, RepoInfo
 
 
 # FILES:
-# ~/.terraform/repos.json
-REPO_CONFIG_PATH = os.path.join(os.environ.get('HOME'), '.terraform', 'repos.json')
-
-
-_import_errors = []
-
-@contextlib.contextmanager
-def importcatch():
-    try:
-        yield
-    except ImportError as err:
-        _import_errors.append(err.name)
-
-with importcatch(): import simplejson
-with importcatch(): from click import echo, group, argument, option
+# ~/.homely/repos.json
+REPO_CONFIG_PATH = os.path.join(os.environ.get('HOME'), '.homely', 'repos.json')
 
 
 class Fatal(Exception):
-    def __init__(self, message, *args, **kwargs):
-        self.message = message
-        super(Fatal, self).__init__(*args, **kwargs)
-
-
-class RepoError(Fatal):
     pass
 
-
-class RepoConfig(object):
-    def __init__(self):
-        # load existing cfg file
-        self.repos = []
-        try:
-            with open(REPO_CONFIG_PATH, 'r') as f:
-                obj = simplejson.loads(f.read())
-                # FIXME: sanity check on the decoded object
-                self.repos = obj
-        except FileNotFoundError as err:
-            pass
-
-    def add_repo(self, info):
-        assert isinstance(info, RepoInfo)
-        commithash = info.commithash
-        modified = False
-        for repo in self.repos:
-            if repo["commithash"] == commithash:
-                # change the local path in the config
-                repo["localpath"] = info.localpath
-                modified = True
-                break
-        if not modified:
-            self.repos.append({"commithash": info.commithash, "localpath": info.localpath})
-        # make dirs needed for repo config
-        makeit = os.path.dirname(REPO_CONFIG_PATH)
-        os.makedirs(makeit, mode=0o755, exist_ok=True)
-        with open(REPO_CONFIG_PATH, 'w') as f:
-            f.write(simplejson.dumps(self.repos, indent=' ' * 4))
-
-
-class RepoInfo(object):
-    localpath = None
-    commithash = None
-
-    def __init__(self, path):
-        # make sure the path is valid
-        if not os.path.exists(path):
-            raise RepoError("%s does not exist" % path)
-        if not os.path.exists(os.path.join(path, '.git')):
-            raise RepoError("%s is not a valid git repo" % path)
-        self.localpath = path
-        # ask git for the commit hash
-        cmd = ['git', '-C', path]
-        cmd.extend(['log', '--pretty=%H', '-1'])
-        self.commithash = subprocess.check_output(cmd).rstrip().decode('utf-8')
-        
 
 def heading(message):
     echo(message)
@@ -90,14 +24,14 @@ def heading(message):
 
 
 @group()
-def tf():
+def homely():
     """
     Single-command dotfile installation.
     """
     pass
 
 
-@tf.command()
+@homely.command()
 @argument('repo_path')
 def add(repo_path):
     '''
@@ -114,7 +48,7 @@ def add(repo_path):
     update_repo(info, pull_first=pull_required)
 
 
-@tf.command()
+@homely.command()
 @argument('identifier')
 def remove(identifier):
     '''
@@ -146,13 +80,13 @@ def update_repo(info, pull_first):
     assert isinstance(info, RepoInfo)
 
 
-@tf.command()
+@homely.command()
 @argument('repo_path', required=False)
 def update(repo_path):
-    raise Exception("TODO: git pull and run the terraform script")  # noqa
+    raise Exception("TODO: git pull and run the homely script")  # noqa
 
 
-@tf.command()
+@homely.command()
 @option('--daily', is_flag=True, help="Update interactively daily")
 @option('--weekly', is_flag=True, help="Update interactively weekly")
 @option('--monthly', is_flag=True, help="Update interactively monthly")
@@ -161,18 +95,16 @@ def updatecheck():
     Interactively update all your repos on a regular basis.
     E.g., add this to your ~/.bashrc:
 
-        terraform updatecheck --weekly
+        homely updatecheck --weekly
     '''
-    raise Exception("TODO: check timestamp in ~/.terraform/last-check")  # noqa
+    raise Exception("TODO: check timestamp in ~/.homely/last-check")  # noqa
     raise Exception("TODO: update all repos if necessary")  # noqa
-    raise Exception("TODO: put new timestamp in ~/.terraform/last-check")  # noqa
+    raise Exception("TODO: put new timestamp in ~/.homely/last-check")  # noqa
 
 def main():
     try:
-        if len(_import_errors):
-            raise Fatal("Missing required modules: %s" % ",".join(_import_errors))
         # FIXME: always ensure git is installed first
-        tf()
-    except Fatal as err:
-        echo("ERROR: %s" % err.message, err=True)
+        homely()
+    except (Fatal, RepoError) as err:
+        echo("ERROR: %s" % err, err=True)
         sys.exit(1)
---
+++ homely/utils.py
@@ -0,0 +1,56 @@
+import os
+import subprocess
+
+import simplejson
+
+
+class RepoError(Exception):
+    pass
+
+
+class RepoConfig(object):
+    def __init__(self):
+        # load existing cfg file
+        self.repos = []
+        try:
+            with open(REPO_CONFIG_PATH, 'r') as f:
+                obj = simplejson.loads(f.read())
+                # FIXME: sanity check on the decoded object
+                self.repos = obj
+        except FileNotFoundError as err:
+            pass
+
+    def add_repo(self, info):
+        assert isinstance(info, RepoInfo)
+        commithash = info.commithash
+        modified = False
+        for repo in self.repos:
+            if repo["commithash"] == commithash:
+                # change the local path in the config
+                repo["localpath"] = info.localpath
+                modified = True
+                break
+        if not modified:
+            self.repos.append({"commithash": info.commithash, "localpath": info.localpath})
+        # make dirs needed for repo config
+        makeit = os.path.dirname(REPO_CONFIG_PATH)
+        os.makedirs(makeit, mode=0o755, exist_ok=True)
+        with open(REPO_CONFIG_PATH, 'w') as f:
+            f.write(simplejson.dumps(self.repos, indent=' ' * 4))
+
+
+class RepoInfo(object):
+    localpath = None
+    commithash = None
+
+    def __init__(self, path):
+        # make sure the path is valid
+        if not os.path.exists(path):
+            raise RepoError("%s does not exist" % path)
+        if not os.path.exists(os.path.join(path, '.git')):
+            raise RepoError("%s is not a valid git repo" % path)
+        self.localpath = path
+        # ask git for the commit hash
+        cmd = ['git', '-C', path]
+        cmd.extend(['log', '--pretty=%H', '-1'])
+        self.commithash = subprocess.check_output(cmd).rstrip().decode('utf-8')
