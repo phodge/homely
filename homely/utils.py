a5e0d40c6181be773ef0d94c295cd43e45aadcd8
Peter Hodge <peterhodge@example.com>
2016-06-20 23:33:16 +1000

    homely.utils

--- homely/utils.py
+++ homely/utils.py
@@ -1,75 +1,196 @@
+import contextlib
 import os
 import subprocess
+import tempfile
 
 import simplejson
 
+CONFIG_DIR = os.path.join(os.environ.get('HOME'), '.homely')
+REPO_CONFIG_PATH = os.path.join(CONFIG_DIR, 'repos.json')
 
-REPO_CONFIG_PATH = os.path.join(os.environ.get('HOME'),
-                                '.homely',
-                                'repos.json')
+
+class JsonError(Exception):
+    pass
+
+
+class JsonConfig(object):
+    jsonpath = None
+    jsondata = None
+
+    def __init__(self):
+        try:
+            with open(self.jsonpath, 'r') as f:
+                data = f.read()
+                if not len(data):
+                    raise FileNotFoundError(None)
+                self.jsondata = simplejson.loads(data)
+                self.checkjson()
+        except FileNotFoundError as err:
+            self.jsondata = self.defaultjson()
+        except simplejson.JSONDecodeError:
+            raise JsonError("%s does not contain valid JSON" % self.jsonpath)
+
+    def checkjson(self):
+        """Child classes should override this method to check if self.jsondata is sane."""
+        raise Exception("This method needs to be overridden")
+
+    def defaultjson(self):
+        """
+        Child classes should override this method to return the default json object for when the
+        config file doesn't exist yet.
+        """
+        raise Exception("This method needs to be overridden")
+
+
+    def writejson(self):
+        # make dirs needed for config file
+        os.makedirs(os.path.dirname(self.jsonpath), mode=0o755, exist_ok=True)
+        # write the config file now
+        with open(self.jsonpath, 'w') as f:
+            f.write(simplejson.dumps(self.jsondata, indent=' ' * 4))
 
 
 class RepoError(Exception):
     pass
 
 
-class RepoConfig(object):
-    def __init__(self):
-        # load existing cfg file
-        self.repos = []
-        try:
-            with open(REPO_CONFIG_PATH, 'r') as f:
-                obj = simplejson.loads(f.read())
-                # FIXME: sanity check on the decoded object
-                self.repos = obj
-        except FileNotFoundError as err:
-            pass
+class RepoListConfig(JsonConfig):
+    jsonpath = REPO_CONFIG_PATH
+    jsondata = None
+
+    def defaultjson(self):
+        return []
+
+    def checkjson(self):
+        assert isinstance(self.jsondata, list)
+        for entry in self.jsondata:
+            assert 'commithash' in entry
+            assert 'localpath' in entry
 
     def add_repo(self, info):
         assert isinstance(info, RepoInfo)
         commithash = info.commithash
         modified = False
-        for repo in self.repos:
+        for repo in self.jsondata:
             if repo["commithash"] == commithash:
                 # change the local path in the config
                 repo["localpath"] = info.localpath
                 modified = True
                 break
         if not modified:
-            self.repos.append({"commithash": info.commithash, "localpath": info.localpath})
-        # make dirs needed for repo config
-        makeit = os.path.dirname(REPO_CONFIG_PATH)
-        os.makedirs(makeit, mode=0o755, exist_ok=True)
-        with open(REPO_CONFIG_PATH, 'w') as f:
-            f.write(simplejson.dumps(self.repos, indent=' ' * 4))
+            self.jsondata.append({"commithash": info.commithash, "localpath": info.localpath})
 
     def find_repo(self, hash_or_path):
-        for repo in self.repos:
+        for repo in self.jsondata:
             if hash_or_path in (repo["commithash"], repo["localpath"]):
-                return repo["commithash"], repo["localpath"]
+                return RepoInfo(repo["localpath"], repo["commithash"])
 
     def find_all(self):
-        for repo in self.repos:
-            yield repo["commithash"], repo["localpath"]
+        for repo in self.jsondata:
+            yield RepoInfo(repo["localpath"], repo["commithash"])
+
+
+class RepoScriptConfig(JsonConfig):
+    jsondata = None
+
+    def __init__(self, info):
+        self.jsonpath = os.path.join(CONFIG_DIR, 'repos', info.commithash + '.json')
+        super(RepoScriptConfig, self).__init__()
+
+    def defaultjson(self):
+        return {
+            # a list of things that were installed on the last run
+            "prevthings": [],
+            "prevchanges": {},
+        }
+
+    def checkjson(self):
+        pass
+
+    def getthings(self):
+        import homely.general
+        for thing in self.jsondata['prevthings']:
+            class_ = thing["class"]
+            kwargs = thing["kwargs"]
+            yield getattr(homely.general, class_)(**kwargs)
+
+    def clearthings(self):
+        self.jsondata["prevthings"] = []
+
+    def addthing(self, thing, changes):
+        self.jsondata["prevthings"].append(thing.asdict())
+        self.jsondata["prevchanges"][thing.uniqueid] = changes
+
+    def getprevchanges(self, uniqueid):
+        return self.jsondata["prevchanges"].get(uniqueid, {})
+
+
+@contextlib.contextmanager
+def saveconfig(cfg):
+    assert isinstance(cfg, JsonConfig)
+    yield
+    cfg.writejson()
 
 
 class RepoInfo(object):
     localpath = None
     commithash = None
 
-    def __init__(self, path):
+    def __init__(self, path, commithash=None):
         # make sure the path is valid
         if not os.path.exists(path):
             raise RepoError("%s does not exist" % path)
         if not os.path.exists(os.path.join(path, '.git')):
             raise RepoError("%s is not a valid git repo" % path)
         self.localpath = path
-        # ask git for the commit hash
-        cmd = ['git', '-C', path]
-        cmd.extend(['log', '--pretty=%H', '-1'])
-        self.commithash = subprocess.check_output(cmd).rstrip().decode('utf-8')
+        if commithash is None:
+            # ask git for the commit hash
+            cmd = ['git', '-C', path]
+            cmd.extend(['log', '--pretty=%H', '-1'])
+            self.commithash = subprocess.check_output(cmd).rstrip().decode('utf-8')
+        else:
+            self.commithash = commithash
 
-    @classmethod
-    def load_from_identifier(class_, identifier):
-        cfg = RepoConfig()
+    @property
+    def shorthash(self):
+        return self.commithash[0:8]
 
+
+@contextlib.contextmanager
+def filereplacer(filepath):
+    """
+    This context manager yields two file pointers:
+
+    orig:
+        a file descriptor as if you had used open(filepath)
+    tmp:
+        a file descriptor as if you had used open(tempfile.mkstemp(), 'w')
+
+    Note that upon successful exiting of the context manager, the file nominated by filepath will
+    be replaced by the temp file. This will be done using file renames, so it is as close to atomic
+    as we can get it.
+
+    If the context block raises an exception, the original file is not changed, and the temp file
+    is deleted.
+    """
+    import shutil
+    # create the tmp dir if it doesn't exist yet
+    tmpdir = os.path.join(CONFIG_DIR, 'tmp')
+    os.makedirs(tmpdir, mode=0o700, exist_ok=True)
+    tmpname = os.path.join(tmpdir, os.path.basename(filepath))
+    try:
+        if os.path.exists(filepath):
+            shutil.copy2(filepath, tmpname)
+        with open(tmpname, 'w') as tmp:
+            try:
+                with open(filepath, 'r') as orig:
+                    yield tmp, orig
+            except FileNotFoundError:
+                yield tmp, None
+    except:
+        with contextlib.suppress(FileNotFoundError):
+            os.unlink(tmpname)
+        raise
+    with contextlib.suppress(FileNotFoundError):
+        os.unlink(filepath)
+    os.rename(tmpname, filepath)
