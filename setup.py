e6c353352c80e730d9df3b45e721dbe395555014
Peter Hodge <peterhodge@example.com>
2016-07-01 00:42:29 +1000

    Get setup.py ready for uploading to pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,29 @@ from setuptools import setup, find_packages
 
 setup(
     name='homely',
-    version='dev',
+    description=('Automate the installation of your personal config files and'
+                 ' favourite tools'),
+    url='https://github.com/toomuchphp/homely/',
+    author='Peter Hodge',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+    ],
+    keywords='dotfiles environment configuration tools utilities automation',
     packages=find_packages(),
-    include_package_data=True,
     install_requires=['simplejson', 'click'],
     entry_points='''
         [console_scripts]
         homely=homely._scripts.homely:main
     ''',
+    # automatic version number using setuptools_scm
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    # TODO: use write_to to put the version in homely.__version__
+    #write_to='homely/__init__.py',
 )
