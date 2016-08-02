a3a4cb7820351dc3ee2a176d3dd60ff3c55022f2
Peter Hodge <peter.hodge@netboxblue.com>
2016-08-03 06:33:05 +1000

    Add homely._vcs.git and unit test for it

---
+++ homely/_vcs/git.py
@@ -0,0 +1,67 @@
+import re
+import os
+import subprocess
+
+import homely._vcs
+
+
+class Repo(homely._vcs.Repo):
+    type = homely._vcs.HANDLER_GIT_v1
+    pulldesc = 'git pull'
+
+    @classmethod
+    def frompath(class_, repo_path):
+        if os.path.isdir(repo_path):
+            if not os.path.isdir(os.path.join(repo_path, '.git')):
+                return
+            return class_(repo_path,
+                          isremote=False,
+                          iscanonical=False,
+                          suggestedlocal=None
+                          )
+        if (repo_path.startswith('ssh://') or
+                repo_path.startswith('https://') or
+                repo_path.startswith('git@')):
+            m = re.match(r'^(https://|git@)github\.com[/:]([^/]+)/([^/]+)',
+                         repo_path)
+            if not m:
+                return
+            _, user, name = m.groups()
+            if name.endswith('.git'):
+                name = name[0:-4]
+            canonical = 'https://github.com/%s/%s.git' % (user, name)
+            return class_(repo_path,
+                          isremote=True,
+                          iscanonical=repo_path == canonical,
+                          suggestedlocal=name,
+                          )
+
+    def pullchanges(self):
+        assert not self.isremote
+        cmd = ['git', 'pull']
+        subprocess.check_call(cmd, cwd=self.repo_path)
+
+    def clonetopath(self, dest):
+        origin = self.repo_path
+        subprocess.check_call(['git', 'clone', origin, dest])
+
+    def getrepoid(self):
+        assert not self.isremote
+        cmd = ['git', 'rev-list', '--max-parents=0', 'HEAD']
+        return (subprocess.check_output(cmd, cwd=self.repo_path)
+                .rstrip()
+                .decode('utf-8'))
+
+    @staticmethod
+    def shortid(repoid):
+        return repoid[0:8]
+
+    def isdirty(self):
+        cmd = ['git', 'status', '--porcelain']
+        out = subprocess.check_output(cmd,
+                                      cwd=self.repo_path,
+                                      stderr=subprocess.STDOUT)
+        for line in out.split(b'\n'):
+            if len(line) and not line.startswith(b'?? '):
+                return True
+        return False
---
+++ test/test_gitvcs.py
@@ -0,0 +1,72 @@
+import re
+import os
+from subprocess import check_call
+
+from pytest import contents
+
+
+def makegitrepo(tmpdir, name):
+    path = os.path.join(tmpdir, name)
+
+    def system(cmd):
+        check_call(cmd, cwd=path)
+
+    os.mkdir(path)
+    system(['git', 'init'])
+    readme = os.path.join(path, 'README.md')
+    contents(readme, "Hello\n")
+    system(['git', 'add', 'README.md'])
+    system(['git', 'commit', '-m', 'Added readme'])
+    return path
+
+
+def test_git(tmpdir):
+    # test loading handler from url
+    from homely._vcs.git import Repo
+    https = 'https://github.com/johnsmith/example.git'
+
+    tests = [
+        'git@github.com:johnsmith/example.git',
+        https,
+    ]
+    for repo_path in tests:
+        vcsrepo = Repo.frompath(repo_path)
+        assert vcsrepo.iscanonical == (repo_path == https)
+        assert vcsrepo.isremote is True
+
+    # check that frompath() works on folders also
+    assert Repo.frompath(tmpdir) is None
+
+    mygit = os.path.join(tmpdir, 'therepo')
+    os.mkdir(mygit)
+    os.mkdir(mygit + '/.git')
+    repo = Repo.frompath(mygit)
+    assert repo is not None
+    assert repo.repo_path == mygit
+    assert repo.iscanonical is False
+    assert repo.isremote is False
+    assert repo.pulldesc == 'git pull'
+
+    # test repo.clonetopath() and repo.isdirty()
+    fake1path = makegitrepo(tmpdir, 'fake1')
+    fake1repo = Repo.frompath(fake1path)
+    assert fake1repo.suggestedlocal == 'fake1'
+    assert re.match(r'^[0-9a-f]{40}$', fake1repo.getrepoid())
+    assert re.match(r'^[0-9a-f]{8}$', fake1repo.shortid(fake1repo.getrepoid()))
+    assert not fake1repo.isdirty()
+    with open(os.path.join(fake1path, 'file.txt'), 'w'):
+        pass
+    assert not fake1repo.isdirty()
+    check_call(['git', 'add', 'file.txt'], cwd=fake1path)
+    assert fake1repo.isdirty()
+
+    clone1path = os.path.join(tmpdir, 'clone1')
+    fake1repo.clonetopath(clone1path)
+    clone1repo = Repo.frompath(clone1path)
+    assert clone1repo.getrepoid() == fake1repo.getrepoid()
+
+    # test repo.pullchanges() by making a change in the original fake
+    assert not os.path.exists(os.path.join(clone1path, 'file.txt'))
+    check_call(['git', 'commit', '-m', 'Added file'], cwd=fake1path)
+    clone1repo.pullchanges()
+    assert os.path.exists(os.path.join(clone1path, 'file.txt'))
