3d942355624c9efb23cd29517bf7a21e8c9cf02b
Peter Hodge <peter.hodge84@gmail.com>
2016-09-09 08:10:00 +1000

    Remove junk test

--- test/system/cmdline.py
+++
@@ -1,5 +0,0 @@
-from pytest import HOMELY, contents, TestRepo, checkrepolist
-
-
-def test_homely_update_check(tmpdir):
-    raise Exception("TODO: test all aspects of 'homely updatecheck'")  # noqa
