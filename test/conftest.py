d6ed246eee7fba8b27ed77d8d7f9a2e40fa0b81f
Peter Hodge <peter.hodge84@gmail.com>
2016-09-16 03:06:03 +1000

    conftest.py: Even more brutal destroying of modules

--- test/conftest.py
+++ test/conftest.py
@@ -55,10 +55,12 @@ def tmpdir(request):
 def HOME(tmpdir):
     home = os.path.join(tmpdir, 'john')
     os.mkdir(home)
-    # NOTE: homely._utils makes use of os.environ['HOME'], so we need to
-    # destroy the module if it has already been loaded, and override the env
-    # variable
-    sys.modules.pop('homely._utils', None)
+    # NOTE: homely._utils makes use of os.environ['HOME'], so we need to destroy any homely modules
+    # that may have imported things based on this ... essentially we blast away the entire module
+    # and reload it from scratch
+    for name in list(sys.modules.keys()):
+        if name.startswith('homely.'):
+            sys.modules.pop(name, None)
     os.environ['HOME'] = home
     return home
 
