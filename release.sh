f9d74e8e036463f167579bf7cafd336cdbe9cec0
Peter Hodge <peter.hodge84@gmail.com>
2016-09-21 16:56:36 +1000

    release.sh script

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+fail() {
+    echo "$@" >&2
+    exit 1
+}
+
+tag="$1"
+shift
+test -n "$tag" || fail "USAGE: $0 TAG"
+
+# make sure there is nothing in git status that could get in the way
+test -n "$(git st -s)" && fail "A clean checkout is required"
+
+git tag "$tag" -m "Tagged $tag" || fail "Couldn't create tag $tag"
+python setup.py sdist bdist_wheel || fail "Build failed"
+twine upload dist/* || "Twine upload failed"
