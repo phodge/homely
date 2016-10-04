deaeb763aa3f7bfcabee3750a29659f119f7d5ce
Peter Hodge <peter.hodge84@gmail.com>
2016-10-04 23:01:34 +1000

    Refactor/cleanup of homely.pipinstall

--- homely/pipinstall.py
+++ homely/pipinstall.py
@@ -1,17 +1,20 @@
+from homely._errors import HelperError
 from homely._engine2 import Helper, Cleaner, getengine
 from homely._utils import haveexecutable
 from homely._ui import isinteractive, system
 
 
-def pipinstall(packagename, which, user=True):
+def pipinstall(packagename, pips=[], *, trypips=[]):
     engine = getengine()
-    for version in which:
-        assert version in (2, 3)
-        helper = PIPInstall(packagename, version, user)
+    for pip in pips:
+        helper = PIPInstall(packagename, pip, mustinstall=True)
+        engine.run(helper)
+    for pip in trypips:
+        helper = PIPInstall(packagename, pip, mustinstall=False)
         engine.run(helper)
 
 
-_known_pips = set()
+_known_pips = {}
 
 
 def _haspkg(pipcmd, name):
@@ -26,24 +29,30 @@ def _haspkg(pipcmd, name):
 
 class PIPInstall(Helper):
     _name = None
-    _version = None
+    _pip = None
+    _pipcmd = None
+    _mustinstall = True
+    # TODO: get rid of this option
     _user = False
 
-    def __init__(self, name, version, user):
+    def __init__(self, name, pip, mustinstall):
         super(PIPInstall, self).__init__()
         self._name = name
-        self._version = version
-        self._user = user
-        self._pipcmd = {2: "pip2", 3: "pip3"}[version]
+        self._mustinstall = mustinstall
+        self._pip = pip
 
-        if self._pipcmd not in _known_pips:
-            if not haveexecutable(self._pipcmd):
-                # FIXME: what type of helpful error should we be raising here?
-                raise Exception("%s executable not found" % self._pipcmd)
-            _known_pips.add(self._pipcmd)
+        try:
+            haveexec = _known_pips[pip]
+        except KeyError:
+            haveexec = haveexecutable(pip)
+            _known_pips[pip] = haveexec
+
+        if haveexec:
+            self._pipcmd = pip
 
     def getcleaner(self):
-        return PIPCleaner(self._name, self._pipcmd)
+        if self._pipcmd is not None:
+            return PIPCleaner(self._name, self._pipcmd)
 
     def pathsownable(self):
         return {}
@@ -52,22 +61,26 @@ class PIPInstall(Helper):
         yield "%s:%s" % (self._pipcmd, self._name)
 
     def isdone(self):
+        if self._pipcmd is None:
+            if not self._mustinstall:
+                return True
+            raise HelperError("%s executable not found" % self._pipcmd)
         return _haspkg(self._pipcmd, self._name)
 
     @property
     def description(self):
-        user = ' --user' if self._user else ''
-        return "%s install %s%s" % (self._pipcmd, self._name, user)
+        return "%s install --user %s" % (self._pipcmd, self._name)
 
     def makechanges(self):
+        if self._pipcmd is None:
+            raise HelperError("%s executable not found" % self._pipcmd)
         cmd = [
             self._pipcmd,
             'install',
             self._name,
+            '--user',
             '--disable-pip-version-check',
         ]
-        if self._user:
-            cmd.append('--user')
         system(cmd)
         factname = 'pipinstall:%s:%s' % (self._pipcmd, self._name)
         self._setfact(factname, True)
@@ -80,7 +93,6 @@ class PIPCleaner(Cleaner):
     def __init__(self, name, pipcmd):
         super(PIPCleaner, self).__init__()
         self._name = name
-        assert pipcmd in ('pip2', 'pip3')
         self._pipcmd = pipcmd
 
     def asdict(self):
