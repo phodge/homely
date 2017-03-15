ee0985270eb2ee2dac41b1be3a3a374466ce5325
Peter Hodge <peter.hodge84@gmail.com>
2017-03-16 08:34:00 +1000

    [9] homely._utils: move async subprocess runner into a separate python3-only script

--- homely/_utils.py
+++ homely/_utils.py
@@ -16,6 +16,7 @@ from homely._vcs import Repo, fromdict
 
 try:
     import asyncio
+    from homely._asyncioutils import _runasync
 except ImportError:
     asyncio = None
 
@@ -194,66 +195,6 @@ def run(cmd, stdout=None, stderr=None, **kwargs):
     finally:
         if devnull is not None:
             devnull.close()
-
-
-def _runasync(stdoutfilter, stderrfilter, cmd, **kwargs):
-    assert asyncio is not None
-
-    @asyncio.coroutine
-    def _runandfilter(loop, cmd, **kwargs):
-        def factory():
-            return FilteringProtocol(asyncio.streams._DEFAULT_LIMIT, loop)
-
-        class FilteringProtocol(asyncio.subprocess.SubprocessStreamProtocol):
-            _stdout = b""
-            _stderr = b""
-
-            def pipe_data_received(self, fd, data):
-                if fd == 1:
-                    if stdoutfilter:
-                        self._stdout = stdoutfilter(self._stdout + data, False)
-                    else:
-                        self.stdout.feed_data(data)
-                elif fd == 2:
-                    if stderrfilter:
-                        self._stderr = stderrfilter(self._stderr + data, False)
-                    else:
-                        self.stderr.feed_data(data)
-                else:
-                    raise Exception("Unexpected fd %r" % fd)
-
-            def pipe_connection_lost(self, fd, exc):
-                if fd == 1:
-                    if stdoutfilter and self._stdout:
-                        stdoutfilter(self._stdout, True)
-                elif fd == 2:
-                    if stderrfilter and self._stderr:
-                        stderrfilter(self._stderr, True)
-                return super().pipe_connection_lost(fd, exc)
-
-        transport, protocol = yield from loop.subprocess_exec(factory,
-                                                              *cmd,
-                                                              **kwargs)
-        process = asyncio.subprocess.Process(transport, protocol, loop)
-
-        # now wait for the process to complete
-        out, err = yield from process.communicate()
-        return process.returncode, out, err
-
-    _exception = None
-
-    def handleexception(loop, context):
-        nonlocal _exception
-        if _exception is None:
-            _exception = context["exception"]
-
-    # FIXME: probably shouldn't be using the main loop here
-    loop = asyncio.get_event_loop()
-    loop.set_exception_handler(handleexception)
-    result = loop.run_until_complete(_runandfilter(loop, cmd, **kwargs))
-    if _exception:
-        raise _exception
-    return result
 
 
 def haveexecutable(name):
