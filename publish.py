3ec2bf081026feca0bede11821ae36939525704f
Peter Hodge <peter.hodge84@gmail.com>
2019-01-02 10:22:48 +1000

    publish.py: Better changelog management

--- publish.py
+++ publish.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import glob
+import os
 import sys
 from datetime import date
 from subprocess import check_call, check_output
@@ -11,27 +12,88 @@ def fail(message):
     sys.exit(1)
 
 
-def updatechangelog(tag):
-    changelog = 'CHANGELOG.rst'
+def updatechangelog(new_tag, latest_tag):
+    # what should the heading at top of changelog be?
+    new_heading = "Version {} - {}".format(
+        new_tag, date.today().strftime("%d %b %Y"))
 
-    with open(changelog) as f:
-        for line in f:
+    changelog_old = 'CHANGELOG.rst'
+    changelog_new = changelog_old + '.new'
+
+    heading_replaced = False
+    replace_next_lines = False
+
+    with open(changelog_old) as f_old, open(changelog_new, 'w') as f_new:
+        for line in f_old:
             if line == 'NEW\n':
-                break
-        else:
-            fail("Heading 'NEW' doesn't appear in {}".format(
-                changelog))
+                # instead of copying this line, replace it with the new heading
+                f_new.write(new_heading)
+                f_new.write('\n')
+                heading_replaced = True
+                replace_next_lines = True
+                continue
 
-    # create the new heading - include the current date
-    heading = "Version {} - {}".format(tag, date.today().strftime("%d %b %Y"))
-    lines = '-' * len(heading)
-    cmd = ['vim', '-u', 'NONE', changelog,
-           '+%s/^NEW$/{}\r{}/'.format(heading, lines),
-           '+wq',
-           ]
-    check_call(cmd)
-    check_call(['git', 'commit', changelog,
-                '-m', 'Update heading in changelog'])
+            if replace_next_lines:
+                replace_next_lines = False
+                f_new.write('-' * len(new_heading))
+                f_new.write('\n')
+
+                if line == '\n':
+                    # a blank line should be re-inserted
+                    f_new.write('\n')
+                if not line.startswith('---'):
+                    # Don't re-insert the dashed line (it will have incorrect
+                    # length). Anything else is unexpected.
+                    fail('"NEW" heading not followed by blank line or "---"')
+
+                continue
+
+            # otherwise, just copy the line to the new file
+            f_new.write(line)
+
+    # if we didn't replace the NEW heading, we should add it
+    if not heading_replaced:
+        with open(changelog_old) as f_old, open(changelog_new, 'w') as f_new:
+            heading_added = False
+            seen_title = False
+            for line in f_old:
+                f_new.write(line)
+                if heading_added:
+                    pass
+                elif line.strip() == 'CHANGELOG':
+                    seen_title = True
+                elif seen_title and line.startswith('==='):
+                    # add the new heading here
+                    f_new.write(new_heading)
+                    f_new.write('\n')
+                    f_new.write('-' * len(new_heading))
+                    f_new.write('\n\n')
+                    f_new.write('* DOCUMENT CHANGES HERE\n')
+                    heading_added = True
+
+        if not heading_added:
+            fail('"NEW" heading not present and was not added automatically')
+
+    # have a go at editing the file directly
+    splitcmds = [
+        # start a new buffer
+        '+new',
+        # give it a name
+        '+silent file === Changes since {} ==='.format(latest_tag),
+        # paste git log output
+        '+silent read !git log --oneline {}..'.format(latest_tag),
+        # delete blank line at top
+        '+normal! ggdd',
+        # modify buffer settings so that vim doesn't ask to save this buffer
+        '+setlocal buftype=nofile',
+        # jump back to original window
+        '+wincmd w',
+    ]
+    check_call(['vim', changelog_new] + splitcmds)
+
+    # move file sideways and commit it
+    os.rename(changelog_new, changelog_old)
+    check_call(['git', 'commit', changelog_old, '-m', 'Update changelog'])
 
 
 def main():
@@ -39,12 +101,18 @@ def main():
     if not len(tag):
         fail("USAGE: {} TAG".format(sys.argv[0]))
 
+    # get the latest tag name
+    revlist = check_output(['git', 'rev-list', '--tags', '--max-count=1'])
+    branches = filter(None, revlist.decode('utf-8').split('\n'))
+    cmd = ['git', 'describe', '--tags'] + list(branches)
+    latest_tag = check_output(cmd).strip().decode('utf-8')
+
     # make sure there is nothing in git status that could get in the way
     output = check_output(['git', 'st', '-s'])
     if len(output):
         fail("ERROR: A clean checkout is required")
 
-    updatechangelog(tag)
+    updatechangelog(tag, latest_tag)
 
     # create the new tag
     check_call(['git', 'tag', tag, '-m', 'Tagged {}'.format(tag)])
