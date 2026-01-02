95fbd2ee6825ca762b2fddec945e8b4a99f23107
Peter Hodge <peter.hodge84@gmail.com>
2026-01-03 00:46:19 +1000

    all_tests_containerised.sh: support for running just one version

--- bin/all_tests_containerised.sh
+++ bin/all_tests_containerised.sh
@@ -10,8 +10,18 @@ try_tests() {
     docker build --pull --build-arg=PYTHON_VERSION=$version -f test/tests.Dockerfile . || fail "Containerised testing failed"
 }
 
-try_tests 3.10
-try_tests 3.11
-try_tests 3.12
+case "$1" in
+	3.1*)
+		try_tests "$1";;
+	ALL)
+		try_tests 3.10
+		try_tests 3.11
+		try_tests 3.12
+		;;
+	*)
+		echo "Usage: $0 { 3.10 | 3.11 | ... | ALL }" >&2
+		exit 2
+		;;
+esac
 
 win "All tests succeeded"
