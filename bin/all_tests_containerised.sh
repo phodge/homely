ac5dc8b47c8ee1614a017234f926d0190a99d323
Peter Hodge <peter.hodge84@gmail.com>
2026-01-03 01:40:33 +1000

    Also test against 3.13 in CI

--- bin/all_tests_containerised.sh
+++ bin/all_tests_containerised.sh
@@ -22,7 +22,7 @@ for arg in "$@"; do
 			targets="$targets $arg"
 			;;
 		ALL)
-			targets="3.10 3.11 3.12"
+			targets="3.10 3.11 3.12 3.13"
 			;;
 		*)
 			echo "Usage: $0 { 3.10 | 3.11 | ... | ALL }" >&2
