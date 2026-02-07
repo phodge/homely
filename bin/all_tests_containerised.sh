06b3a91f037fc410a2bfbb7ee695a0d9c4f537e4
Peter Hodge <peter.hodge84@gmail.com>
2026-02-08 06:57:16 +1000

    all_tests_containerised should warn when no arguments given

--- bin/all_tests_containerised.sh
+++ bin/all_tests_containerised.sh
@@ -3,12 +3,21 @@ fail() { local z="$?"; echo -e "\033[01;31m[$(date +'%H:%M:%S%P')] FAIL[$z]: $@\
 win() { echo -e "\033[01;32m[$(date +'%H:%M:%S%P')] $@\033[0m" >&2; }
 say() { echo -e "\033[01;35m[$(date +'%H:%M:%S%P')] $@\033[0m" >&2; }
 
+exit_usage() {
+    echo "Usage: $0 { 3.10 | 3.11 | ... | ALL }" >&2
+    exit 2
+}
+
 try_tests() {
     version=$1
     shift
     say "Testing under python $version"
     docker build $build_opts --build-arg=PYTHON_VERSION=$version -f test/tests.Dockerfile . || fail "Containerised testing failed"
 }
+
+if [ "$#" -lt 1 ]; then
+    exit_usage
+fi
 
 build_opts=""
 targets=""
@@ -25,8 +34,7 @@ for arg in "$@"; do
 			targets="3.10 3.11 3.12 3.13 3.14"
 			;;
 		*)
-			echo "Usage: $0 { 3.10 | 3.11 | ... | ALL }" >&2
-			exit 2
+            exit_usage
 			;;
 	esac
 done
