2ba1777f499af0b7791161dfead8e9145d0cd3ee
Peter Hodge <peter.hodge84@gmail.com>
2026-05-03 08:09:13 +1000

    bin/ci/check_versions.sh: I think CI version of grep is different

--- bin/ci/check_versions.sh
+++ bin/ci/check_versions.sh
@@ -12,7 +12,7 @@ expected_version="$1"
 shift
 
 if [ -n "$expected_version" ]; then
-	if ! grep "^version = \"$expected_version\"\$" homely/__init__.py &>/dev/null; then
+	if ! grep "^version = \"$expected_version\"" homely/__init__.py &>/dev/null; then
 		echo "ERROR: expected 'version = \"$expected_version\"' in homely/__init__.py" >&2
 		exit 3
 	fi
