44ea8e28dc2a982fa5f79f1ba69c329471d71ea3
Peter Hodge <peter.hodge84@gmail.com>
2026-05-03 08:39:04 +1000

    ***bin/ci/check_versions.sh: bring back simple redirect

--- bin/ci/check_versions.sh
+++ bin/ci/check_versions.sh
@@ -18,7 +18,7 @@ if [ -n "$expected_version" ]; then
 	grep "^version = \"$expected_version\"" homely/__init__.py
 	echo $?
 	echo '----------'
-	if ! grep "^version = \"$expected_version\"" homely/__init__.py; then
+	if ! grep "^version = \"$expected_version\"" homely/__init__.py >/dev/null; then
 		echo "ERROR: expected 'version = \"$expected_version\"' in homely/__init__.py" >&2
 		exit 3
 	else
