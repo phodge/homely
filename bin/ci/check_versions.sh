c6d16c0fc6f64a86c9ff3919b142621489c0b877
Peter Hodge <peter.hodge84@gmail.com>
2026-05-03 08:24:53 +1000

    ***bin/ci/check_versions.sh: many debug

--- bin/ci/check_versions.sh
+++ bin/ci/check_versions.sh
@@ -12,7 +12,15 @@ expected_version="$1"
 shift
 
 if [ -n "$expected_version" ]; then
-	if ! grep "^version = \"$expected_version\"" homely/__init__.py &>/dev/null; then
+	echo '----------'
+	cat homely/__init__.py
+	echo '----------'
+	grep "^version = \"$expected_version\"" homely/__init__.py
+	echo $?
+	echo '----------'
+	if grep "^version = \"$expected_version\"" homely/__init__.py &>/dev/null; then
+		echo "GOOD: version in homely/__init__.py matches expected version '$expected_version'"
+	else
 		echo "ERROR: expected 'version = \"$expected_version\"' in homely/__init__.py" >&2
 		exit 3
 	fi
